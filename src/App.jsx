98d23339e113262dd3af4dbca7f0405a2b81f38c
Yash824-YT <yashtalekar824@gmail.com>
2026-07-26 21:18:43 +0530

    To upload the new file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+    <div className="text-center">
+      <h1>Welcome</h1>
+      <Navbar />
+      <Footer />
+    </div>
+    </>
+  )
+}
+
+export default App
